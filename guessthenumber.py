d9a3fd4712116ff51aa2667379ee6d822de06843
yuvaraj-gunturu <107745159+yuvaraj-gunturu@users.noreply.github.com>
2022-06-18 12:16:50 +0530

    Add files via upload

---
+++ guessthenumber.py
@@ -0,0 +1,15 @@
+# Guess The Number
+import random
+def guess(x):
+    random_number = random.randint(1,x)
+    guess =0
+    while guess!=random_number:
+        guess = int(input(f"Guess a number between 1 and {x}:"))
+        if guess<random_number:
+            print("Too low")
+        elif guess>random_number:
+            print("Too high")
+    print(f"You have guess the correct number{random_number}")
+
+guess(10)
+        
